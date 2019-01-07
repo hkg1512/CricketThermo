af02dca7a8ccb393b995791cdb417d8f7a128708
Gangadharan <ganghar@ent.bhicorp.com>
2019-01-07 09:59:57 +0530

    Intial Commit - Version 1.0.0.0

---
+++ app/src/main/java/com/hkgapps/ganghar/cricketthermo/MainActivity.java
@@ -0,0 +1,70 @@
+package com.hkgapps.ganghar.cricketthermo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ImageView ivThermo;
+    Button btnCalculate;
+    EditText etChirp;
+    TextView tvDescription;
+    TextView tvResult;
+    CheckBox cbFahrenheit;
+
+    DecimalFormat formatter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        btnCalculate = findViewById(R.id.btnCalculate);
+        etChirp = findViewById(R.id.etChirp);
+        tvDescription = findViewById(R.id.tvDescription);
+        tvResult = findViewById(R.id.tvResult);
+        cbFahrenheit = findViewById(R.id.cbFahrenheit);
+        ivThermo = findViewById(R.id.ivThermo);
+
+        tvResult.setVisibility(View.GONE);
+        formatter = new DecimalFormat("#0.00");
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvResult.setVisibility(View.GONE);
+
+                if (etChirp.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter a value in the field", Toast.LENGTH_SHORT).show();
+                } else {
+                    String strChirp = etChirp.getText().toString().trim();
+                    etChirp.getText().clear();
+                    int iChirp = Integer.parseInt(strChirp);
+                    double dResult;
+                    String strResult;
+
+                    if (cbFahrenheit.isChecked()) {
+                        dResult = iChirp + 40.0;
+                        strResult = "The calculated temperature (in Farenhiet): " + formatter.format(dResult) + " deg F";
+                    } else {
+                        dResult = (iChirp / 3.0) + 4;
+                        strResult = "The calculated temperature (in Celsius): " + formatter.format(dResult) + " deg C";
+                    }
+
+                    tvResult.setText(strResult);
+                    tvResult.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+}
